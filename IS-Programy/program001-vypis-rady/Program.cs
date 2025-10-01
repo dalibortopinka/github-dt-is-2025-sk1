ec89fd6936cbf17f7b4ada18fb4c455b05a71b54
Dalibor Topinka <dalibor.topinka@tul.cz>
2025-10-01 13:21:11 +0200

    hahha uz to jde #3

--- IS-Programy/program001-vypis-rady/Program.cs
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -1,33 +1,28 @@
-﻿internal class Program
+﻿string again = "a";
+while (again == "a")
 {
-    private static void Main(string[] args)
+    Console.Clear();
+    Console.WriteLine("**********");
+    Console.WriteLine("***Výpis řady čísel***");
+    Console.WriteLine("*********");
+    Console.WriteLine("***Dalibor Topinka***");
+    Console.WriteLine("********");
+    Console.WriteLine();
+
+
+    //Vstup hodnoty do programu řešený správně
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+
+    while (!int.TryParse(Console.ReadLine(), out first))
     {
-        string again = "a";
-        while (again == "a")
-        {
-            Console.Clear();
-            Console.WriteLine("**********");
-            Console.WriteLine("***Výpis řady čísel***");
-            Console.WriteLine("*********");
-            Console.WriteLine("***Dalibor Topinka***");
-            Console.WriteLine("********");
-            Console.WriteLine();
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu.");
+    
+ }
 
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskni a");
+    again = Console.ReadLine();
 
-            //Vstup hodnoty do programu řešený správně
-            Console.Write("Zadejte první číslo řady (celé číslo): ");
-            int first;
-
-            while (!int.TryParse(Console.ReadLine(), out first))
-            {
-                Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu.");
-
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Pro opakování programu stiskni a");
-            again = Console.Readline();
-
-        }
-    }
 }
+
