30afb990cd7e504dcf3665e53069356610a8054f
Dalibor Topinka <dalibor.topinka@tul.cz>
2025-10-01 10:24:51 +0200

    #3

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,28 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("**********");
+    Console.WriteLine("***Výpis řady čísel***");
+    Console.WriteLine("*********");
+    Console.WriteLine("***Dalibor Topinka***");
+    Console.WriteLine("********");
+    Console.WriteLine();
+
+
+    //Vstup hodnoty do programu řešený správně
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu.");
+    
+ }
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskni a");
+    again = Console.Readline();
+
+}
+
