344f237d545894695b605232d9dfb2a1dcfd3456
Dalibor Topinka <dalibor.topinka@tul.cz>
2025-10-22 09:38:36 +0200

    #6 Největší ze tří čísel - výchozí projekt

---
+++ program003b-nejvetsi-ze-tri-cisel/Program.cs
@@ -0,0 +1,50 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("****************************");
+    Console.WriteLine("***** Seřazení dvou čísel *****");
+    Console.WriteLine("****************************");
+    Console.WriteLine("******* Dalibor Topinka ********");
+    Console.WriteLine("****************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu - špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    //Vstup hodnoty do programu - řešený správně (určím hodnoty pro a a pro b)
+    Console.Write("Zadejte celé číslo - hodnota A:  ");
+    int a;
+
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu znovu: ");
+    }
+
+
+    Console.Write("Zadejte celé číslo - hodnota B:  ");
+    int b;
+
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu znovu: ");
+    }
+
+    Console.WriteLine();
+    int pom;          //pomocná (sklenička)
+    if (a > b)
+    {
+        pom = a;
+        a = b;
+        b = pom;
+        Console.WriteLine("Došlo k prohození proměnných");
+    }
+
+    Console.WriteLine($"Seřazená čísla: {a}, {b}");
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+
+}
