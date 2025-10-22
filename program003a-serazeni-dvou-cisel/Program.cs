a8c3ae45180c3171b64f9a8aad7cc5bb7da87fa0
Dalibor Topinka <dalibor.topinka@tul.cz>
2025-10-22 09:36:37 +0200

    #6 Seřazení dvou čísel

--- program003a-serazeni-dvou-cisel/Program.cs
+++ program003a-serazeni-dvou-cisel/Program.cs
@@ -13,19 +13,38 @@ while (again == "a")
     //Console.Write("Zadejte první číslo řady: ");
     //int first = int.Parse(Console.ReadLine());
 
-    //Vstup hodnoty do programu - řešený správně
-    Console.Write("Zadejte hodnotu (celé číslo): ");
-    int first;
+    //Vstup hodnoty do programu - řešený správně (určím hodnoty pro a a pro b)
+    Console.Write("Zadejte celé číslo - hodnota A:  ");
+    int a;
 
-    while (!int.TryParse(Console.ReadLine(), out first))
+    while (!int.TryParse(Console.ReadLine(), out a))
     {
         Console.Write("Nezadali jste celé číslo. Zadejte hodnotu znovu: ");
     }
 
 
+    Console.Write("Zadejte celé číslo - hodnota B:  ");
+    int b;
+
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu znovu: ");
+    }
+
+    Console.WriteLine();
+    int pom;          //pomocná (sklenička)
+    if (a > b)
+    {
+        pom = a;
+        a = b;
+        b = pom;
+        Console.WriteLine("Došlo k prohození proměnných");
+    }
+
+    Console.WriteLine($"Seřazená čísla: {a}, {b}");
+
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
     again = Console.ReadLine();
-
 
 }
