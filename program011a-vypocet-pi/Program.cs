6451888c41ec9585771b796f773975d68a22bdda
Dalibor Topinka <dalibor.topinka@tul.cz>
2025-12-03 09:38:09 +0100

    #13 Výpočet pí

---
+++ program011a-vypocet-pi/Program.cs
@@ -0,0 +1,38 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("****************************");
+    Console.WriteLine("***** Výpočet pí *****");
+    Console.WriteLine("****************************");
+    Console.WriteLine("******* Dalibor Topinka ********");
+    Console.WriteLine("****************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte hodnotu (reálné číslo): ");
+    double presnost; //datový typ double - reálná čísla
+
+    while (!double.TryParse(Console.ReadLine(), out presnost))
+    {
+        Console.Write("Nezadali jste reálné číslo. Zadejte hodnotu znovu: ");
+    }
+
+double i=1;
+double piCtvrt = 1;
+double znamenko = 1;
+
+while ((1/i)>= presnost) {
+    i = i+2;
+    znamenko = -znamenko;
+    piCtvrt = piCtvrt + znamenko * (1/i);
+}
+       
+    double pi = 4 * piCtvrt;
+    Console.WriteLine("Vypočítané pí: {0}", pi);
+    
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+
+}
